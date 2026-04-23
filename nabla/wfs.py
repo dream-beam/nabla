7ed6d220fab9a426092f846667dcb84bee6365aa
Antoine Islegen-Wojdyla <awojdyla@lbl.gov>
2026-04-23 10:11:25 -0700

    simulations

---
+++ nabla/wfs.py
@@ -0,0 +1,22 @@
+# simulations
+
+# reconstruction
+## base algorithm
+# Frankot Chelapa
+
+
+# display
+## just beam
+## phase
+## slopes
+## equivalent mirror height
+## colors phase
+
+# caustics
+
+#Analysis
+## basis decompositions
+## fit source distance
+## PSF / min beam size
+
+# automate / auto pick
